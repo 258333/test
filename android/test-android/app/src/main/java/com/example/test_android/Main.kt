246548f2581f86ad15238cf6891ffb9b051f424d
Hongwei <3052681940@qq.com>
2024-05-20 20:08:06 +0800

    添加主函数

---
+++ android/test-android/app/src/main/java/com/example/test_android/Main.kt
@@ -0,0 +1,9 @@
+package com.example.test_android
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyComposable() {
+    // ...
+    println("hello")
+}
--- springboot/test-idea/Main.java
+++ springboot/test-idea/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    // 添加其他代码
+    // ...
+    public void test(){
+        System.out.println("This is a test method.");
+    }
 }
